c72c66e4c9a21747c63c4389a4e0d435a8af8bf4
tsar <your.email@example.com>
2020-03-10 16:37:39 +0400

    Create django project

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    """ Модель категорий """
+    name = models.CharField(verbose_name = "Имя", max_length = 100)
+    slug = models.SlugField("url", max_length = 100)
+
+    def __str__(self): # возвращаем имя категории для корректного отображения Category type
+        return self.name
+
+    class Meta:  # Указатель корректное отображение
+        verbose_name = "Категория" # НА русском языке
+        verbose_name_plural = "Категории" # Убираем S вконце
+
